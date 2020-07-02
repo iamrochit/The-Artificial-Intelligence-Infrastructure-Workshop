737ea0952f3537c09f7a4b302573410e8ab2151b
iamrochit <iamrochitjain@gmail.com>
2020-07-03 00:50:31 +0530

    Tested and Verified. No change required.

--- Chapter06/Exercise06.03/ORC.scala
+++ Chapter06/Exercise06.03/ORC.scala
@@ -17,7 +17,6 @@ df_census_csv.write.orc("F:/Chapter06/Data/Output/census_csv.orc")
 // Using JSON Data frame
 df_census_json.write.orc("F:/Chapter06/Output/Data/census_json.orc")
 
-
 // Reading ORC file
 val df_census_orc = spark.read.orc("F:/Chapter06/Data/Output/census_csv.orc")
 df_census_orc.show()
